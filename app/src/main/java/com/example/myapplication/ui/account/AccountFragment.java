aed72ad46fd4f97b0bc2ba6d2c71d69dfedb5379
saini1998 <sainiaaryaman1998@gmail.com>
2020-04-09 23:02:47 +0530

    commit...

--- app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
@@ -24,12 +24,61 @@ public class AccountFragment extends Fragment {
                 ViewModelProviders.of(this).get(AccountViewModel.class);
         View root = inflater.inflate(R.layout.fragment_account, container, false);
         final TextView textView = root.findViewById(R.id.text_account);
-        accountViewModel.getText().observe(this, new Observer<String>() {
+        accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+
+        final TextView textView1 = root.findViewById(R.id.oh);
+        accountViewModel.getText1().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView1.setText(s);
+            }
+        });
+
+        final TextView textView2 = root.findViewById(R.id.apc);
+        accountViewModel.getText2().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView2.setText(s);
+            }
+        });
+
+        final TextView textView3 = root.findViewById(R.id.pm);
+        accountViewModel.getText3().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView3.setText(s);
+            }
+        });
+
+        final TextView textView4 = root.findViewById(R.id.ms);
+        accountViewModel.getText4().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView4.setText(s);
+            }
+        });
+
+        final TextView textView5 = root.findViewById(R.id.sas);
+        accountViewModel.getText5().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView5.setText(s);
+            }
+        });
+        final TextView textView6 = root.findViewById(R.id.faq);
+        accountViewModel.getText6().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView6.setText(s);
+            }
+        });
+
         return root;
     }
 }
